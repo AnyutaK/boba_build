9961d2d1f4fdd59fe6d7835842599d03438ae96a
AnyutaK <anyuta.mitmpl2024@learner.manipal.edu>
2026-07-30 01:19:44 +0530

    added options.jsx

---
+++ src/data/options.jsx
@@ -0,0 +1,32 @@
+export const teaBases = [
+  "Milk Tea",
+  "Matcha",
+  "Taro",
+  "Brown Sugar",
+  "Green Tea",
+  "Strawberry Tea"
+];
+
+export const sweetnessLevels = [
+  "0%",
+  "25%",
+  "50%",
+  "75%",
+  "100%"
+];
+
+export const iceLevels = [
+  "No Ice",
+  "Less Ice",
+  "Normal Ice",
+  "Extra Ice"
+];
+
+export const toppings = [
+  "Tapioca Pearls",
+  "Popping Boba",
+  "Jelly",
+  "Grass Jelly",
+  "Cheese Foam",
+  "Red Bean"
+];
